d2b2f7f1fc9edc6966819e2e4604c96f12d8cef0
Julian <juliantcarr@gmail.com>
2021-02-28 21:57:21 +0200

    added a method showMsg();

--- src/hellogithub/HelloGithub.java
+++ src/hellogithub/HelloGithub.java
@@ -4,6 +4,9 @@ package hellogithub;
 public class HelloGithub {
 
     public static void main(String[] args) {
+        new HelloGithub().showMsg();
+    }
+    public void showMsg(){
         System.out.println("Hello GitHub");
     }
     
