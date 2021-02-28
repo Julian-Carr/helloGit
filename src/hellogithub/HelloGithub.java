d34fb65fff85e2ac274288e810abbd1a9454c6c5
Julian <juliantcarr@gmail.com>
2021-02-28 22:03:56 +0200

    Enhancement: new class and bye method

--- src/hellogithub/HelloGithub.java
+++ src/hellogithub/HelloGithub.java
@@ -5,9 +5,12 @@ public class HelloGithub {
 
     public static void main(String[] args) {
         new HelloGithub().showMsg();
+        new HelloGithub().showBye();
     }
     public void showMsg(){
         System.out.println("Hello GitHub");
     }
-    
+     public void showBye(){
+        System.out.println("Goodbye GitHub");
+     }
 }
